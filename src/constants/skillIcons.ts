d50d97f0ee4fbfa55349413bcf139fca32d15eda
KevinU20221275 <U20221275@univo.edu.sv>
2025-04-30 11:46:42 -0600

    move utility constants to constant directory

---
+++ src/constants/skillIcons.ts
@@ -0,0 +1,53 @@
+import Html from '../components/icons/Html.astro';
+import Css from '../components/icons/Css.astro';
+import JavaScript from '../components/icons/JavaScript.astro';
+import React from '../components/icons/React.astro';
+import NodeJs from '../components/icons/Node.astro';
+import ExpressJs from '../components/icons/Express.astro';
+import MongoDb from '../components/icons/MongoDB.astro';
+import Git from '../components/icons/Git.astro';
+import GitHubSkill from '../components/icons/GitHubSkill.astro';
+import Tailwind from '../components/icons/Tailwind.astro';
+import Bootstrap from '../components/icons/Bootstrap.astro';
+import Php from '../components/icons/Php.astro';
+import MySql from '../components/icons/MySql.astro';
+import Python from '../components/icons/Python.astro';
+import Csharp from '../components/icons/Csharp.astro';
+import SqLite from '../components/icons/SqLite.astro';
+import AspDotNet from '../components/icons/AspDotNet.astro';
+import SqlServer from '../components/icons/SqlServer.astro';
+import TypeScript from '../components/icons/TypeScript.astro';
+import Sql from '../components/icons/Sql.astro';
+import Zustand from '../components/icons/Zustand.astro';
+import Astro from '../components/icons/Astro.astro';
+import PostgresSQL from "../components/icons/PostgresSQL.astro"
+import NextJS from '../components/icons/NextJS.astro';
+import Jquery from '@/components/icons/Jquery.astro';
+
+export const SKILLS_ICONS : Record<string, any> = {
+    "HTML": Html,
+    "CSS": Css,
+    "Javascript": JavaScript,
+    "Typescript": TypeScript,
+    "React": React,
+    "NodeJs": NodeJs,
+    "ExpressJs": ExpressJs,
+    "MongoDB": MongoDb,
+    "Git": Git,
+    "GitHub": GitHubSkill,
+    "Tailwind": Tailwind,
+    "Bootstrap": Bootstrap,
+    "SQLServer": SqlServer,
+    "PHP": Php,
+    "MySQL": MySql,
+    "Python": Python,
+    "CSharp": Csharp,
+    "SQLite": SqLite,
+    "AspDotNet": AspDotNet,
+    "Sql": Sql,
+    "Zustand": Zustand,
+    "Astro": Astro,
+    "NextJS" : NextJS,
+    "PostgresSQL" : PostgresSQL,
+    "Jquery" : Jquery
+}
---
+++ src/constants/themes.js
@@ -0,0 +1,11 @@
+export const ICONS = {
+    MOON: "moon",
+    SUN: "sun",
+    SYSTEM: "system",
+}
+
+export const THEMES = {
+    LIGHT: "light",
+    DARK: "dark",
+    SYSTEM: "system",
+}
