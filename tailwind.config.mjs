ab30a18892470495827799a35374b3ad2a9f84c0
KevinU20221275 <U20221275@univo.edu.sv>
2025-04-30 11:38:21 -0600

    add dark/light themes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: "class",
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+
+				light : {
+					surface: "#5F6DE0",
+					surfaceAlt: "#202872",
+					textPrimary: '#2A348E',
+      				textSecondary: '#4B5563',
+					textLigth: "#1A1A1A",
+					white: "#F5F5F5",
+					accent: "#1B1464",
+      				accentSecondary: "#8E7A2A",
+				},
+
+				dark : {
+					surface: "#232344",
+					surfaceAlt: "#343444",
+					hover: "#121222",
+					accent: "#9D59EF",
+					accentSecondary: "#F8DB9C",
+					textPrimary: "#9D59EF",
+					textSecondary: "#B1B1BA",
+					textDarkLigth: "#494F5D",
+					projectCardBg: "#2243",
+					border: "#02081A",
+				},
+			}
+		},
 	},
 	plugins: [],
 }
